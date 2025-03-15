dca88c7f292d788fad7c9b523f003ce095e5a15e
ThiagoCruzeiro <thiago.morais01@cs.cruzeirodosul.edu.br>
2025-03-15 10:47:40 -0300

    Add files via upload

---
+++ Dia15.js
@@ -0,0 +1,46 @@
+console.log(`Olá mundo`);
+
+let x = 5; x = 10; console.log(x);
+
+const y= 20; console.log(y);
+
+function soma (a,b) {return  a + b}
+
+if (x > 10 ) {console.log("maior");} else {(console.log("menor"));}
+
+let x = 9;
+
+if(x > 10){console.log("maior");} else{console.log("menor");}
+
+for (let i = 0; i < array.length; i++) { console.log(i);}
+
+let x = 11;
+if (x > 10) {console.log("Maior que 10");}
+if (x > 10) {console.log("Maior");} else{console.log("Menor ou igual");}
+if (x > 10) {console.log("Maior que 10");}
+    else if (x === 10) {console.log("Igual a 10");}
+    else {console.log("Menor que 10");}
+
+
+let dia = "Segunda"
+switch (dia) {
+    case "Segunda": console.log("Trabalahar");break; default: console.log("Descandar");break;}
+
+
+for (let i = 0; i < 5; i++) {console.log(i);}
+for (let i = 0; i < 10; i++) {if (i===5) continue; {console.log(i);};}
+
+const arr = [1,2,3]; console.log(arr[3]);
+
+const array2D = [
+    [1,2,3], //primeira linha
+    [4,5,6] //segunda linha
+];
+
+console.log(array2D[0][1]); //saida: 2 (primeira linha, segunda coluna)
+console.log(array2D[1][2]); //saida: 6 (segunda linha, terceira coluna)
+
+
+
+
+
